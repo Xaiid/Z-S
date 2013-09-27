5356ca1a7fb3e589cb7abdb0d9adefa6f7714f68
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-09-27 14:36:43 -0500

    Walls correctly displayed

--- public/javascript/crafty/entities/wall.js
+++ public/javascript/crafty/entities/wall.js
@@ -1,10 +1,10 @@
 // A wall is an entity that works as an actor and is solid
-ZombieWorld.Entity.wall = function(options){ 
+ZombieWorld.Entity.Wall = function(options){ 
   return Crafty.e(ZombieWorld.properties.solid_components + 'Wall')
   .color('rgb(0, 0, 0)')
   .attr({
-    x: options.x,
-    y: options.y,
+    x: (options.x * ZombieWorld.map.title.width),
+    y: (options.y * ZombieWorld.map.title.height),
     w: ZombieWorld.map.title.width,
     h: ZombieWorld.map.title.height
   });
--- public/javascript/crafty/scene.js
+++ public/javascript/crafty/scene.js
@@ -2,5 +2,29 @@ ZombieWorld.Scene.runScene = function(world){
   //Set background initial color
   Crafty.background(world.options.color);
 
+  Crafty.scene(world.options.name, function(){
+    //Track occupied spots
+    this.occupied = new Array(ZombieWorld.map.width);
+    for (var i = 0; i < ZombieWorld.map.width; i++) {
+      this.occupied[i] = new Array(ZombieWorld.map.height);
+      for (var y = 0; y < ZombieWorld.map.height; y++) {
+        this.occupied[i][y] = false;
+      }
+    }
+
+
+    //Create walls
+    for (var x = 0; x < ZombieWorld.map.width; x++) {
+      for (var y = 0; y < ZombieWorld.map.height; y++) {
+        var border = x === 0 || x === ZombieWorld.map.width - 1 || y === 0 || y === ZombieWorld.map.height - 1;
+
+        if (border) {
+          ZombieWorld.Entity.Wall({x: x, y: y});
+          this.occupied[x][y] = true;
+        } 
+      }
+    }
+  });
+
   Crafty.scene(world.options.name);
 };
