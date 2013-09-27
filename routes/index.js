f483a30290f7124ecca2051237a26259ed1857ff
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-09-27 10:54:51 -0500

    Fixed routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 //All the routes are specified in here
 //var controller = require('mycontroller');
 
-modules.exports = function(app){
+var navigation = require('../controllers/navigation_controller');
+
+module.exports = function(app){
   //app.method('route', controller.method.bind(controller));
+  app.get('/', navigation.index.bind(navigation));
 };
