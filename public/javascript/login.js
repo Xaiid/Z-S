5ef1ae804a3b70cd009133eb4fa403b339e540ec
Jeaneth Farmer <xaid27@gmail.com>
2013-09-27 15:50:25 -0500

    finish login process

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,13 +1,24 @@
 $( function(){
   $('#loginForm').submit(function(event){
-       console.log(asd);
-    $.ajax({
+    
+    var username = $('#username').val();
+    var arena   = $('#arena').val();
+
+   var request =  $.ajax({
       url: '/login',
       method: 'POST',
-      data: {username: $('#username').val(), password: 1234},
-    }).done(function(data){
-       console.log(data);
+      data: {username: username, password: 1234},
     });
+
+    request.done(function(data){
+       console.log(data);
+       ZombieWorld.socket.emit('Create player', {username: username, arena: arena});
+    });
+
+    request.error(function(err){
+       console.log(err.responseText);
+    });
+
     event.preventDefault();
   });
 });
