7f545522a211e0dbfdc803779fc1e8c9120b52ec
Jeaneth Farmer <xaid27@gmail.com>
2013-09-27 11:26:32 -0500

    added passport and auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,29 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+module.exports = {
+
+  init: function(){
+
+    passport.serializeUser(function(user, done) {
+      done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done) {
+      done(null, obj);
+    });
+
+  },
+
+  localStrategy: function(req, res) {
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    return new LocalStrategy(function (username, password, done) {
+      if(error) {  return done(error); }
+      return done(err, username);
+    }); // new LocalStrategy
+
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 //All the routes are specified in here
 //var controller = require('mycontroller');
 
-var navigation = require('../controllers/navigation_controller');
+var authController = require('../controllers/authController');
+var navigation     = require('../controllers/navigation_controller');
 
 module.exports = function(app){
   //app.method('route', controller.method.bind(controller));
   app.get('/', navigation.index.bind(navigation));
+
+  app.post('/accounts/login/', authController.authenticate());
+  app.get('/accounts/logout/', authController.logout());
 };
