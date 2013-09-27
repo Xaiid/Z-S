ff0937c16f35271a9866cd51c9f0acfa63c00303
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-09-27 11:22:01 -0500

    Created 3 main pages

--- controllers/navigation_controller.js
+++ controllers/navigation_controller.js
@@ -1,5 +1,13 @@
 module.exports = {
   index: function(req, res){
-    res.render('index');
+    res.render('navigation/index');
+  },
+
+  login: function(req, res){
+    res.render('navigation/login');
+  },
+
+  game: function(req, res){
+    res.render('navigation/game');
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ var navigation     = require('../controllers/navigation_controller');
 
 module.exports = function(app){
   //app.method('route', controller.method.bind(controller));
-  app.get('/', navigation.index.bind(navigation));
 
   app.post('/accounts/login/', authController.login.bind(authController));
   app.get('/accounts/logout/', authController.logout.bind(authController));
+
+  app.get('/', navigation.index.bind(navigation));
+  app.get('/login', navigation.login.bind(navigation));
+  app.get('/game', navigation.game.bind(navigation));
 };
