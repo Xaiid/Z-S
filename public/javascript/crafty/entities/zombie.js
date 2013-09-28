53ee7ab5a82493c3dfbd1cf1cbb65bd71db8df4e
Jeaneth Farmer <xaid27@gmail.com>
2013-09-28 06:50:35 -0500

    added listenTo to zombie entity

--- public/javascript/crafty/entities/zombie.js
+++ public/javascript/crafty/entities/zombie.js
@@ -1,5 +1,5 @@
 ZombieWorld.Entity.zombie = function(zombie){
-  return Crafty.e("2D, Canvas, SpriteAnimation, Mouse, zombies, " + zombie)
+ var zombieProto = Crafty.e("2D, Canvas, SpriteAnimation, Mouse, PlayerControls, " + zombie)
         .attr({
           x: Crafty.math.randomInt(100,130),
           y: Crafty.math.randomInt(100,130)
@@ -16,4 +16,8 @@ ZombieWorld.Entity.zombie = function(zombie){
        .animate("walk_up", 0,  3, 2)
        .animate("walk_down", 0, 0 , 2);
         //.stopOnSolids()
+        if(!zombieProto.control){
+          zombieProto.listenTo();
+        }
+        return zombieProto;
 };
