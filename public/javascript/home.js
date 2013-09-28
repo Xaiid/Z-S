cabd7aa4f1cfc75a91653e26ee953122653dd460
Jaime Gonzalez <jaime.gonzalez@tangosource.com>
2013-09-28 18:15:43 -0500

    added scroll jqeury stuff and styling to that

---
+++ public/javascript/home.js
@@ -0,0 +1,15 @@
+$( function(){
+
+  $('#introduction .next').on('click',function(){
+      $cont = $('#introduction');
+      scroll = $cont.scrollLeft();
+      $cont.animate({scrollLeft: scroll + $cont.width()}, 500);
+  });
+  $('#introduction .prev').on('click',function(){
+      $cont = $('#introduction');
+      scroll = $cont.scrollLeft();
+      $cont.animate({scrollLeft: scroll - $cont.width()}, 500);
+  });
+
+});
+
