c933e72fdfa9ce99bfe6d8f4de5ed1604f5f9936
Jeaneth Farmer <xaid27@gmail.com>
2013-09-27 22:36:14 -0500

    added click binding in grass to move zombie

--- public/javascript/crafty/entities/land/grass.js
+++ public/javascript/crafty/entities/land/grass.js
@@ -10,7 +10,48 @@ ZombieWorld.land.grass = function(cb){
     for(var i = 0; i < ZombieWorld.map.width; i++) {
       //generate the grass along the y-axis
       for(var j = 0; j < ZombieWorld.map.height; j++) {
-        Crafty.e("2D, Canvas, grass1").attr({x: i * 30, y: j * 30});
+        Crafty.e("2D, Canvas, grass1, Mouse").attr({x: i * 30, y: j * 30})
+        .bind('Click', function(e){
+          var zombie = Crafty('zombie1');
+          var endX = Math.floor(e.realX) -25;
+          var endY = Math.floor(e.realY) -25;
+          var directionX;
+          var directionY; 
+
+          var walk = function(cord, end, direction){
+
+          zombie.stop().animate(direction, 15, -1);
+
+            var interval =  setInterval(function(){
+              if(zombie[cord] != end){
+
+                if(zombie[cord] < end){
+                  return zombie[cord]++;
+                }else{
+                  return zombie[cord]--;
+                }
+              }else{
+                zombie.stop();
+                walkY();
+                clearInterval(interval); 
+              }
+
+            }, 30);
+
+          };
+
+          directionX = zombie.x < endX ? 'walk_right' : 'walk_left';
+          walk('x', endX, directionX);
+
+          var walkY = function(){
+            if(zombie.y != endY){
+              directionY =  zombie.y > endY ? 'walk_up' : 'walk_down';
+              walk('y', endY, directionY);
+            }
+          };
+
+        });
+
         sendCb();
       }
     }
