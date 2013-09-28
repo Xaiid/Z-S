742933484cdc01fc5bd195cc64f479a8d41a82d9
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-09-28 02:24:50 -0500

    Added four way to the zombie entity

--- public/javascript/crafty/entities/zombie.js
+++ public/javascript/crafty/entities/zombie.js
@@ -1,49 +1,59 @@
 ZombieWorld.Entity.zombie = function(zombie){
-  return Crafty.e("2D, Canvas, Controls, Collision, SpriteAnimation, ZombieControls, " + zombie)
+  return Crafty.e("2D, Canvas, Controls, Collision, Fourway, SpriteAnimation, ZombieControls, " + zombie)
         .attr({
           x: Crafty.math.randomInt(100,130),
           y: Crafty.math.randomInt(100,130)
         })
+        .fourway(ZombieWorld.properties.zombie.speed)
+        .stopOnSolids()
         .requires('Keyboard')
-        .ZombieControls()
-                            //x, y, frames
         .animate("walk_left", 0 , 1,  2)
         .animate("walk_right", 0 , 2 ,2)
         .animate("walk_up", 0,  3, 2)
         .animate("walk_down", 0, 0 , 2)
+        .bind('NewDirection', function(data) {
+          if (data.x > 0) {
+            this.animate('walk_right', ZombieWorld.properties.animation_speed, -1);
+          } else if (data.x < 0) {
+            this.animate('walk_left', ZombieWorld.properties.animation_speed, -1);
+          } else if (data.y > 0) {
+            this.animate('walk_down', ZombieWorld.properties.animation_speed, -1);
+          } else if (data.y < 0) {
+            this.animate('walk_up', ZombieWorld.properties.animation_speed, -1);
+          } else {
+            this.stop();
+          }
+        })
+        .stopOnSolids()
         .bind("EnterFrame", function(e) {
           if(this.isDown("LEFT_ARROW")) {
-            if(!this.isPlaying("walk_left")){
-              this.stop().animate("walk_left", 10);
-            }
+            ZombieWorld.socket.emit('Move', {x: this.x, y: this.y, to: "LEFT_ARROW"});
           } else if(this.isDown("RIGHT_ARROW")) {
-            if(!this.isPlaying("walk_right")){
-              this.stop().animate("walk_right", 10);
-            }
+            ZombieWorld.socket.emit('Move', {x: this.x, y: this.y, to: "RIGHT_ARROW"});
           } else if(this.isDown("UP_ARROW")) {
-            if(!this.isPlaying("walk_up")){
-              this.stop().animate("walk_up", 10);
-            }
+            ZombieWorld.socket.emit('Move', {x: this.x, y: this.y, to: "UP_ARROW"});
           } else if(this.isDown("DOWN_ARROW")) {
-            if(!this.isPlaying("walk_down")){
-              this.stop().animate("walk_down", 10);
-            }
+            ZombieWorld.socket.emit('Move', {x: this.x, y: this.y, to: "DOWN_ARROW"});
           }
-        }).bind("keyup", function(e) {
-          this.stop();
-        })
-        .onHit("wall_left", function() {
-          this.x += this._speed;
-          this.stop();
-        }).onHit("wall_right", function() {
-          this.x -= this._speed;
-          this.stop();
-        }).onHit("wall_bottom", function() {
-          this.y -= this._speed;
-          this.stop();
-        }).onHit("wall_top", function() {
-          this.y += this._speed;
-          this.stop();
+        //.bind("EnterFrame", function(e) {
+          //if(this.isDown("LEFT_ARROW")) {
+            //if(!this.isPlaying("walk_left")){
+              //this.stop().animate("walk_left", 10);
+            //}
+          //} else if(this.isDown("RIGHT_ARROW")) {
+            //if(!this.isPlaying("walk_right")){
+              //this.stop().animate("walk_right", 10);
+            //}
+          //} else if(this.isDown("UP_ARROW")) {
+            //if(!this.isPlaying("walk_up")){
+              //this.stop().animate("walk_up", 10);
+            //}
+          //} else if(this.isDown("DOWN_ARROW")) {
+            //if(!this.isPlaying("walk_down")){
+              //this.stop().animate("walk_down", 10);
+            //}
+          //}
+        //}).bind("keyup", function(e) {
+          //this.stop();
         });
-        
 };
