b6c9bb49de56a9b4a35970b26184c7ca33000005
Narciso Guillen <narciso.guillen@tangosource.com>
2013-09-28 21:24:03 -0500

    Sinc shoots

--- public/javascript/crafty/components/playerControls.js
+++ public/javascript/crafty/components/playerControls.js
@@ -63,6 +63,7 @@ ZombieWorld.Component.PlayerControls = Crafty.c('PlayerControls', {
   bullet: function(){
     if(!this.shoot){
       this.shoot = true;
+      ZombieWorld.socket.emit('Shoot', {player: ZombieWorld.currentPlayer.username, facing: this.facing});
 
       var bullet;
       var self   = this;
@@ -79,7 +80,6 @@ ZombieWorld.Component.PlayerControls = Crafty.c('PlayerControls', {
         for(x1; x1 > 0 ; x1--){
           i1+= 10;
           _.delay(function(pos){
-            console.log(pos);
             bullet.x = pos;
           }, i1, x1);
         }
@@ -130,6 +130,12 @@ ZombieWorld.Component.PlayerControls = Crafty.c('PlayerControls', {
   },
 
   listenTo: function(){
+    var self = this;
+
+    ZombieWorld.socket.on('Shoot', function(data){
+      self.facing = data.facing;
+      self.bullet2(data.player);
+    });
 
     ZombieWorld.socket.on('Move player', function(data){
 
@@ -170,6 +176,75 @@ ZombieWorld.Component.PlayerControls = Crafty.c('PlayerControls', {
       ZombieWorld.currentPlayer.Enemy[data.who].Entity.animate(data.to, 10);
 
     });
-  }
+  },
+
+  bullet2: function(username){
+    var self = ZombieWorld.players[username].Entity;
+
+    if(!self.shoot){
+      self.shoot = true;
+
+      var bullet;
+
+      setTimeout(function(){
+        self.shoot = false;
+      }, ZombieWorld.properties.bullet_speed);
+
+
+      if(self.facing==='left'){
+        bullet = ZombieWorld.Entity.Bullet({x: self.x, y:self.y, w: 5, h:2});
+        var x1 = self.x;
+        var i1 = 0;
+        for(x1; x1 > 0 ; x1--){
+          i1+= 10;
+          _.delay(function(pos){
+            bullet.x = pos;
+          }, i1, x1);
+        }
+      }
+
+      if(self.facing ==='up'){
+        bullet = ZombieWorld.Entity.Bullet({x: self.x + 13, y:self.y, w:2, h:5});
+        var y1 = self.y;
+        var i2 = 0;
+        for(y1; y1 > 0 ; y1--){
+          i2 +=10;
+          _.delay(function(pos){
+            bullet.y = pos;
+          }, i2 , y1);
+        }
+      }
+
+
+      if(self.facing ==='right'){
+        bullet = ZombieWorld.Entity.Bullet({x: self.x + 25, y:self.y, w: 5, h: 2});
+        var x2 = self.x + 25;
+        var maxX = ZombieWorld.map.width * ZombieWorld.map.title.width;
+        var i3 = 0;
+        for(x2; x2 < maxX ; x2++){
+          i3 +=10;
+          _.delay(function(pos){
+            bullet.x = pos;
+          }, i3 , x2);
+        }
+      }
+
+      if(self.facing ==='down'){
+        bullet = ZombieWorld.Entity.Bullet({x: self.x + 15, y:self.y, w: 2, h: 5});
+        var y2 = self.y;
+        var maxY = ZombieWorld.map.height * ZombieWorld.map.title.height;
+        var i4 = 0;
+        for(y2; y2 < maxY ; y2++){
+          i4 +=10;
+          _.delay(function(pos){
+            bullet.y = pos;
+          }, i4 , y2);
+        }
+      }
+
+
+    }
+
+  },
 
 });
